437d697ba155e0e5ecdc91d102a7f229193597e1
Azariagmt <azariagebremichael10@gmail.com>
2021-09-23 15:29:01 +0300

    modifies to work on any assigned port

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def home():
     return "Hello Flask in Docker"
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0',debug=True, port=port)
